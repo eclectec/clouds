40593cce2b791fc63546ee1645e7ddc297c8292c
Nick <nick@nicks-mbp.lan>
2024-11-05 14:43:05 -0800

    Add original files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# clouds
+The clouds project is utilized to scrape data from api endpoints to simulate streaming
+
+## Build
+```
+docker build -t cloud .
+```
+
+## Broker Options
+The project can submit data to both kafka or redis topics. The broker is set in the docker compose environment variable for BROKER
---
+++ main.py
@@ -0,0 +1,90 @@
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka import Producer
+from os import getenv
+import redis
+import requests
+import logging
+import json
+import time
+
+message_broker = getenv("BROKER", "kafka")
+broker_port = getenv("PORT", "9093")
+topic = getenv("TOPIC", "rumble")
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        logging.error('Message delivery failed: {}'.format(err))
+    else:
+        logging.warning('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+def create_kafka_topic():
+    admin_client = AdminClient({'bootstrap.servers': f'''kafka:{broker_port}'''})
+    new_topics = [NewTopic(topic, num_partitions=1, replication_factor=1)]
+
+    fs = admin_client.create_topics(new_topics)
+
+    # Wait for each operation to finish.
+    for r_topic, f in fs.items():
+        try:
+            f.result()  # The result itself is None
+            print("Topic {} created".format(r_topic))
+        except Exception as e:
+            print("Failed to create topic {}: {}".format(r_topic, e))
+
+
+def process_opensky(data):
+    plot_list = []
+    plots = data["states"]
+    for plot in plots:
+        plot_list.append(
+            {
+                "hex": plot[0],
+                "category": plot[17],
+                "lat": plot[6],
+                "lon": plot[5],
+                "gs": plot[9]
+            }
+        )
+    
+    return plot_list
+
+
+def main():
+    time.sleep(10)
+    url = getenv("URL", None)
+
+    if url:
+        broker = None
+        if(message_broker == "kafka"):
+            create_kafka_topic()
+            broker = Producer({'bootstrap.servers': f'''kafka:{broker_port}'''})
+        else:
+            broker = redis.Redis(host="redis", port=broker_port, db=0)
+
+        while True:
+            air_traffic = None
+            data = requests.get(url)
+            json_data = json.loads(data.text)
+            if "airplanes" in url:
+                air_traffic = json_data["ac"]
+            elif "opensky" in url:
+                air_traffic = process_opensky(json_data)
+            elif "opendata" in url:
+                air_traffic = json_data["aircraft"]
+            else:
+                air_traffic = [{
+                    "message": "hello there"
+                }]
+
+            if air_traffic:
+                for plot in air_traffic:
+                    if(message_broker == "kafka"):
+                        broker.produce(topic, json.dumps(plot), callback=delivery_report)
+                    else:
+                        broker.publish(topic, json.dumps(plot))
+            time.sleep(10)
+
+if __name__ == "__main__":
+    main()
